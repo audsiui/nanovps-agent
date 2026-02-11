99429043090c7a70c882a12758f84dad8cbae457
audsiui <audsiui@outlook.com>
2026-02-11 09:32:35 +0800

    添加 Alpine 镜像构建工作流，更新 .gitignore

---
+++ images/Alpine/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# 1. 如果没密钥，生成一下
+[ ! -f /etc/ssh/ssh_host_rsa_key ] && ssh-keygen -A
+
+# 2. 如果给了密码变量，改 root 密码 (利用 busybox 自带功能)
+[ -n "$ROOT_PASSWORD" ] && echo "root:$ROOT_PASSWORD" | chpasswd
+
+# 3. 启动 OpenRC
+exec /sbin/init
